d452903bcd75789848a0313481c8411ce13fa73d
jinlovespho <msjchr922@gmail.com>
2024-05-13 09:54:50 +0000

    20240513 re run singleframe baseline dpt

---
+++ pretrained_weights/get_weights.sh
@@ -0,0 +1,2 @@
+wget https://download.europe.naverlabs.com/ComputerVision/CroCo/CroCo_V2_ViTBase_BaseDecoder.pth
+wget https://download.europe.naverlabs.com/ComputerVision/CroCo/CroCo_V2_ViTLarge_BaseDecoder.pth
